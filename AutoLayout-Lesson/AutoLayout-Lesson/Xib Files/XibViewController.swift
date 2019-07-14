b754e82866323c87dc376d9b3a13ebeb69eb2d14
zhangmingkai <zhangmingkai19892013@gmail.com>
2019-07-14 17:15:34 +0800

    add autolayout examples

---
+++ AutoLayout-Lesson/AutoLayout-Lesson/Xib Files/XibViewController.swift
@@ -0,0 +1,30 @@
+//
+//  XibViewController.swift
+//  AutoLayout-Lesson
+//
+//  Created by mingkai on 2019/7/14.
+//  Copyright © 2019年 mingkai. All rights reserved.
+//
+
+import UIKit
+
+class XibViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
